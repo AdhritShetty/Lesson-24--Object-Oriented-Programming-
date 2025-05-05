57ed4868a937d1cd3218c9beb0424721119cfdf6
AdhritShetty <adhritshetty2009@gmail.com>
2025-05-05 20:46:46 +0000

    class

---
+++ class.py
@@ -0,0 +1,5 @@
+class student:
+    grade=10
+    print(" I am a student of grade 10")
+
+ob=student()
---
+++ vehicle.py
@@ -0,0 +1,7 @@
+class vehicle :
+    def __init__(self,max_speed,mileage):
+     self.max_speed=max_speed
+     self.mileage=mileage
+modelx=vehicle(240,18)
+print("the maximum speed of the vehicle is",modelx.max_speed)                            
+print("the mileage of the vehicle is",modelx.mileage)  
