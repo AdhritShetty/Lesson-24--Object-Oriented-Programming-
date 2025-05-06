3c5f9ebf7327c240832d93d4c61d94ec13071244
AdhritShetty <adhritshetty2009@gmail.com>
2025-05-06 20:47:33 +0000

    class

---
+++ dogs.py
@@ -0,0 +1,9 @@
+class dogs :
+    def __init__(self,tall,security,aggressive):
+     self.tall=tall
+     self.security=security
+     self.aggresive=aggressive
+modelx=dogs("st._benard","German Shepherd","Chihuaha")
+print("the tall dog is the",modelx.tall)                            
+print("the security dog is the",modelx.security) 
+print("the aggressive dog is the",modelx.aggresive) 
